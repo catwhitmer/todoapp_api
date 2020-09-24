dfaaad08766b1c3916ca9375d91479aad32c6d3a
catwhitmer <catwhitmer@gmail.com>
2020-09-24 08:20:00 -0400

    added basic RESTful actions

---
+++ app/controllers/api/v1/todos_controller.rb
@@ -0,0 +1,33 @@
+class Api::TodosController < ApplicationController
+
+    def index
+
+    end
+
+    def show
+
+    end
+
+    def create
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+
+    private
+
+    def set_todo
+
+    end
+
+    def todo_params
+
+    end
+end
