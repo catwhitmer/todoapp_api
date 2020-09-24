9064fd684bc87ae4bfab5adb1002a3b729813f66
catwhitmer <catwhitmer@gmail.com>
2020-09-24 08:26:20 -0400

    added all RESTfull actions to the todos_controller

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,33 +1,48 @@
 class Api::TodosController < ApplicationController
 
     def index
+        @todos = Todo.all 
 
+        render json: @todos
     end
 
     def show
+        set_todo
 
+        render json: @todo
     end
 
     def create
-
+        @todo = Todo.new(todo_params)
+        if @todo.save
+            render json: @todo
+        else
+            render json: { errors: @todo.errors.full_messages}
+        end
     end
 
     def update
+        set_todo
+        @todo.update(todo_params)
 
+        render json: @todo
     end
 
     def destroy
+        todo = Todo.find(params["id"])
+        todo.destroy
 
+        render json: todo
     end
 
 
     private
 
     def set_todo
-
+        @todo = Todo.find_by(id: params[:id])
     end
 
     def todo_params
-
+        params.require(:todo).permit(:notes)
     end
 end
