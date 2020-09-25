2382f0bbdcb2cc979ced9531d45eb6462242036b
catwhitmer <catwhitmer@gmail.com>
2020-09-25 08:32:14 -0400

    added some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+todo = Todo.create([{notes: 'walk the dog'}, {notes: 'do laundry'}, {notes: 'buy groceries'}])
